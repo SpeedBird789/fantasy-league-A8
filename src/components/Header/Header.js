477c88ad86e942df69243ed7ce2f2c213c5a3514
SpeedBird789 <asifmahmoud787@gmail.com>
2022-03-25 19:31:20 +0000

    player card added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h1>Dream XI</h1>
+            <h2>Make your personalised XI</h2>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const {id, name, price, image} = props.product;
+    return (
+        <div className='product'>
+            <img src={image} alt="" />
+            <div className='product-info'>
+                <h2 className='product-name'>{name}</h2>
+                <h3>Price: £{price}</h3>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
